7d467c21a6fd573f4541ff877ac89374e0005ad0
byeongraeahn <xanbrx88@naver.com>
2019-02-27 14:19:57 +0900

    수정

---
+++ src/app/Thread/ThreadTest4.java
@@ -0,0 +1,49 @@
+package app.Thread;
+
+
+public class ThreadTest4 {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        Thread mother = new Thread(atm, "mother");
+        Thread son = new Thread(atm, "son");
+        mother.start();
+        son.start();
+    }
+}
+
+class ATM implements Runnable {
+    private long depositeMoney = 10000;
+
+    public void run() {
+        synchronized (this) {
+
+            for (int i = 0; i < 10; i++) {
+                notify();
+                try {
+                    wait();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (getDepositeMoney() <= 0)
+                    break;
+                withDraw(1000);
+            }
+        }
+    }
+
+    public void withDraw(long howMuch) {
+        if (getDepositeMoney() > 0) {
+            depositeMoney -= howMuch;
+            System.out.print(Thread.currentThread().getName() + " , ");
+            System.out.printf("잔액 : %,d 원 %n", getDepositeMoney());
+        } else {
+            System.out.print(Thread.currentThread().getName() + " , ");
+            System.out.println("잔액이 부족합니다.");
+        }
+    }
+
+    public long getDepositeMoney() {
+        return depositeMoney;
+    }
+}
