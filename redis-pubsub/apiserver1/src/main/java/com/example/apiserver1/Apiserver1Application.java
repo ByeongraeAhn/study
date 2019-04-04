f3e02f173d0b4184887e9ed37ebb60346fdab206
byeongraeahn <xanbrx88@naver.com>
2019-04-04 15:46:54 +0900

    서버1에 리스너 제거

--- redis-pubsub/apiserver1/src/main/java/com/example/apiserver1/Apiserver1Application.java
+++ redis-pubsub/apiserver1/src/main/java/com/example/apiserver1/Apiserver1Application.java
@@ -5,28 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @SpringBootApplication
 public class Apiserver1Application {
-
-	@Bean
-	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
-
-		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
-
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter() {
-		return new MessageListenerAdapter();
-	}
 
 	@Bean
 	StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
