809bb38cdf3fc1633ea35dde6777f23f395c7352
byeongraeahn <xanbrx88@naver.com>
2019-08-05 17:35:00 +0900

    클래스 형식으로 수정

--- reactjs/movie_app/src/App.js
+++ reactjs/movie_app/src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <Movie />
-    </div>
-  );
+const movies = ["Matrix", "Full Metal Jacket", "Oldboy", "Star Wars"];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie title={movies[0]} />
+        <Movie title={movies[1]} />
+        <Movie title={movies[2]} />
+        <Movie title={movies[3]} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- reactjs/movie_app/src/Movie.js
+++ reactjs/movie_app/src/Movie.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Movie.css";
 
-function Movie() {
-  return (
-    <div>
-      <MoviePoster />
-      <h1>hello this is a movie</h1>
-    </div>
-  );
+class Movie extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <MoviePoster />
+        <h1>hello this is a movie</h1>
+      </div>
+    );
+  }
 }
 
-function MoviePoster() {
-  return <img src="https://img.hankyung.com/photo/201904/03.19271312.1.jpg" />;
+class MoviePoster extends Component {
+  render() {
+    return (
+      <img src="https://img.hankyung.com/photo/201904/03.19271312.1.jpg" />
+    );
+  }
 }
 
 export default Movie;
