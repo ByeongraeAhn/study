7ca7fd4791a24669a202001f396234022272ea5f
byeongraeahn <xanbrx88@naver.com>
2020-01-22 18:30:26 +0900

    시큐리티 베이직 프로젝트 추가

---
+++ spring-security-basic-master/src/main/java/me/whiteship/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -0,0 +1,27 @@
+package me.whiteship.demospringsecurityform;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableAsync
+public class DemoSpringSecurityFormApplication {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
+	}
+
+}
---
+++ spring-security-basic-master/src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
@@ -0,0 +1,86 @@
+package me.whiteship.demospringsecurityform.form;
+
+import me.whiteship.demospringsecurityform.account.Account;
+import me.whiteship.demospringsecurityform.account.AccountContext;
+import me.whiteship.demospringsecurityform.account.AccountRepository;
+import me.whiteship.demospringsecurityform.account.UserAccount;
+import me.whiteship.demospringsecurityform.book.BookRepository;
+import me.whiteship.demospringsecurityform.common.CurrentUser;
+import me.whiteship.demospringsecurityform.common.SecurityLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.concurrent.Callable;
+
+@Controller
+public class SampleController {
+
+    @Autowired SampleService sampleService;
+
+    @Autowired AccountRepository accountRepository;
+
+    @Autowired BookRepository bookRepository;
+
+    @GetMapping("/")
+    public String index(Model model, @CurrentUser Account account) {
+        if (account == null) {
+            model.addAttribute("message", "Hello Spring Security");
+        } else {
+            model.addAttribute("message", "Hello, " + account.getUsername());
+        }
+
+        return "index";
+    }
+
+    @GetMapping("/info")
+    public String info(Model model) {
+        model.addAttribute("message", "Info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("message", "Hello " + principal.getName());
+        sampleService.dashboard();
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("message", "Hello Admin, " + principal.getName());
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user(Model model, Principal principal) {
+        model.addAttribute("message", "Hello User, " + principal.getName());
+        model.addAttribute("books", bookRepository.findCurrentUserBooks());
+        return "user";
+    }
+
+    @GetMapping("/async-handler")
+    @ResponseBody
+    public Callable<String> asyncHandler() {
+        SecurityLogger.log("MVC");
+        return () -> {
+            SecurityLogger.log("Callable");
+            return "Async Handler";
+        };
+    }
+
+    @GetMapping("/async-service")
+    @ResponseBody
+    public String asyncService() {
+        SecurityLogger.log("MVC, before async service");
+        sampleService.asyncService();
+        SecurityLogger.log("MVC, after async service");
+        return "Async Service";
+    }
+
+}
