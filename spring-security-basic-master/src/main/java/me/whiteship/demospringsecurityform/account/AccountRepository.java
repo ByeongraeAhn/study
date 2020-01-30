83199b42135c08c0af4e8bdcbaf350536eb0a8e0
byeongraeahn <xanbrx88@naver.com>
2020-01-30 18:42:57 +0900

    시큐리티 강의 준비

--- spring-security-basic-master/src/main/java/me/whiteship/demospringsecurityform/account/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package me.whiteship.demospringsecurityform.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    Account findByUsername(String username);
-}
