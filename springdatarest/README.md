5eb92f1eda4396b2d024c78eabc3ba9850b70102
anbr <mybatis@lotte.net>
2020-06-12 14:28:48 +0900

    스웨거 추가

---
+++ springdatarest/README.md
@@ -0,0 +1,4 @@
+# SpringDatarREST + Swagger
+
+- 스프링 데이터 레스트를 통한 API를 스웨거를 통해 확인할 수 있다.
+- http://localhost:8080/swagger-ui.html
--- springdatarest/src/main/java/com/example/demo/DemoApplication.java
+++ springdatarest/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
