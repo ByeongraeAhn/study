3966100d4c5c94a29ee2f504eccf5c50bd7a556a
anbr <mybatis@lotte.net>
2020-07-28 18:03:14 +0900

    스트림 추가

--- etc/src/app/stream/StreamTest1.java
+++ etc/src/app/stream/StreamTest1.java
@@ -1,11 +1,14 @@
 package app.stream;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 
@@ -14,7 +17,7 @@ public class StreamTest1 {
 
     public static void main(String[] args) {
 
-        //////////////////////////////////// [생성 관련]
+        //////////////////////////////////// [생성하기]
 
         // 배열 스트림
         String[] arr = new String[] { "a", "b", "c" };
@@ -67,8 +70,46 @@ public class StreamTest1 {
         Stream<String> streamString2 = Stream.of("Python", "Go", "Swift");
         Stream<String> concat = Stream.concat(streamString1, streamString2); // [Java, Scala, Groovy, Python, Go, Swift]
 
-        //////////////////////////////////// [가공 관련]
 
+
+
+
+        //////////////////////////////////// [가공하기]
+
+        List<String> names = Arrays.asList("Eric", "Elena", "Java");
+
+        //Filtering
+        Stream<String> filterStream = names.stream().filter(name -> name.contains("a")); // [Elena, Java]
+
+        //Mapping
+        Stream<String> mappingStream = names.stream().map(String::toUpperCase); // [ERIC, ELENA, JAVA]
+
+        //FlatMap
+        //flatMap 은 중첩 구조를 한 단계 제거하고 단일 컬렉션으로 만들어주는 역할을 한다
+        List<List<String>> nestedList = Arrays.asList(Arrays.asList("a"), Arrays.asList("b")); // [[a], [b]]
+        List<String> flatList = nestedList.stream().flatMap(Collection::stream).collect(Collectors.toList()); // [a, b]
+
+        //Sorting
+        List<String> lang = Arrays.asList("Java", "Scala", "Groovy", "Python", "Go", "Swift");
+        lang.stream().sorted().collect(Collectors.toList()); // [Go, Groovy, Java, Python, Scala, Swift]
+
+        //Iterating
+        //peek 은 그냥 확인해본다는 단어 뜻처럼 특정 결과를 반환하지 않는 함수형 인터페이스 Consumer 를 인자로 받는다
+        //스트림 내 요소들 각각에 특정 작업을 수행할 뿐 결과에 영향을 미치지 않는다.
+        int sum = IntStream.of(1, 3, 5, 7, 9).peek(System.out::println).sum();
+
+
+
+
+        
+        //////////////////////////////////// [결과 만들기]
+
+        //Calculating
+        long streamCount = IntStream.of(1, 3, 5, 7, 9).count();
+        long streamSum = LongStream.of(1, 3, 5, 7, 9).sum();
+        //만약 스트림이 비어 있는 경우 count 와 sum 은 0을 출력하면 된다. 하지만 평균, 최소, 최대의 경우에는 표현할 수가 없기 때문에 Optional 을 이용해 리턴합니다.
+        OptionalInt min = IntStream.of(1, 3, 5, 7, 9).min();
+        OptionalInt max = IntStream.of(1, 3, 5, 7, 9).max();
 
     }
 
