f1eeefdacae4a87d8c0fe7fbd218f99efac57f50
anbr <mybatis@lotte.net>
2021-03-11 17:21:08 +0900

    ENUM 수정

--- etc/src/app/enumm/EnumTest.java
+++ etc/src/app/enumm/EnumTest.java
@@ -1,21 +1,30 @@
 package app.enumm;
 
 enum PowerSwitch {
-    //
-    ON("켜짐"), OFF("꺼짐");
+    ON("켜짐",true), OFF("꺼짐",false);
 
     private String krName;
+    private boolean flag;
 
     private PowerSwitch() {
         //
     }
 
-    private PowerSwitch(String krName) {
+    private PowerSwitch(String krName, boolean flag) {
         this.krName = krName;
+        this.flag = flag;
     }
 
     public String getKrName() {
         return krName;
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String krName) {
+        this.krName = krName;
     }
 
     public PowerSwitch opposite() {
@@ -31,14 +40,20 @@ enum PowerSwitch {
 public class EnumTest {
     public static void main(String[] args) {
         PowerSwitch powerSwitch = PowerSwitch.ON;
+        displayByPowerSwitch(powerSwitch);
         displayByPowerSwitch(powerSwitch.opposite());
+
     }
 
-    public static void displayByPowerSwitch(PowerSwitch powerSwitch) {
+    private static void displayByPowerSwitch(PowerSwitch powerSwitch) {
         if (powerSwitch == PowerSwitch.ON) {
             System.out.println("Power On");
+            System.out.println(powerSwitch.getKrName());
+            System.out.println(powerSwitch.getFlag());
         } else {
             System.out.println("Power Off");
+            System.out.println(powerSwitch.getKrName());
+            System.out.println(powerSwitch.getFlag());
         }
     }
 }
